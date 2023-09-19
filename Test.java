f67aa09ce30c1e6d5d9147847de8d112bcb6fda3
obinnanprogrammed <obinnanwakwue10@gmail.com>
2023-09-18 20:36:34 -0500

    added LinkedList

--- Test.java
+++ Test.java
@@ -1,5 +1,9 @@
+import java.util.*;
+
 public class Test {
     public static void main(String[] args) {
         System.out.println("Hello!");
+        LinkedList<String> steps = new LinkedList<String>();
+        System.out.println(steps);
     }
 }
